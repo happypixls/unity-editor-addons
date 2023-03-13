41fb2c2d3eb0d1b1bbc69f3dd1901bf19243f2e6
Mohamed ElZayyat <elzayat.mohamed@hotmail.com>
2023-03-13 22:52:46 +0100

    Fixing major bugs and adding features
    - Added Calculators class for normalizing and denormalizing values given variable ranges
    - Fixed major bugs with different types of script creations

--- Editor/NamespaceResolver.cs
+++ Editor/NamespaceResolver.cs
@@ -1,17 +1,17 @@
-﻿#if UNITY_EDITOR
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEditor;
-using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace HappyPixels.EditorAddons
 {
     public enum FileType
     {
-        None,
+        None = -1,
         CSharpScript = 0,
         InterfaceFile = 1,
         EnumFile = 2,
@@ -32,7 +32,7 @@ namespace HappyPixels.EditorAddons
         
         private static string GenerateNamespace(string metaFilePath)
         {
-            var segmentedPath = $"{Path.GetDirectoryName(metaFilePath)}".Split(new[] { '\\' }, StringSplitOptions.None);
+            var segmentedPath = $"{Path.GetDirectoryName(metaFilePath)}".Split(new[] { Path.DirectorySeparatorChar }, StringSplitOptions.None);
 
             var generatedNamespace = "";
             var finalNamespace = "";
@@ -59,28 +59,40 @@ namespace HappyPixels.EditorAddons
 
         private static void OnWillCreateAsset(string metaFilePath)
         {
-            var fileName = Path.GetFileNameWithoutExtension(metaFilePath);
-
-            
-            if (metaFilePath.EndsWith(".cs.meta") && CurrentlyCreatedFile == FileType.None) 
-                CurrentlyCreatedFile = FileType.CSharpScript;
-            else if (metaFilePath.EndsWith(".asmdef.meta"))
-            {
-                Debug.Log("Attempting to create assembly definition file");
-                CurrentlyCreatedFile = FileType.Asmdef;
-            }
-            else
-                return;
-            
             if (string.IsNullOrEmpty(EditorSettings.projectGenerationRootNamespace))
             {
                 Debug.LogError("Root Namespace is not set in project settings, Namespace resolver is aborted.");
                 return;
             }
+            
+            var fileName = Path.GetFileNameWithoutExtension(metaFilePath);
 
-            Debug.Log("CALLING CREATION OF SCRIPTS");
-            FileTemplatesGenerators[CurrentlyCreatedFile].Invoke(metaFilePath, fileName);
-            CurrentlyCreatedFile = FileType.None; //This is set in order not to conflict with cs interface creation or enums 
+            if (metaFilePath.EndsWith(".cs.meta") )
+            {
+                switch (CurrentlyCreatedFile)
+                {
+                    case FileType.None:
+                        FileTemplatesGenerators[FileType.CSharpScript].Invoke(metaFilePath, fileName);
+                        break;
+                    default:
+                        FileTemplatesGenerators[CurrentlyCreatedFile].Invoke(metaFilePath, fileName);
+                        break;
+                }
+
+                CurrentlyCreatedFile = FileType.None;
+                return;
+            }
+            
+            if (metaFilePath.EndsWith(".asmdef.meta"))
+            {
+                Debug.Log("Attempting to create assembly definition file");
+                CurrentlyCreatedFile = FileType.Asmdef;
+                FileTemplatesGenerators[CurrentlyCreatedFile].Invoke(metaFilePath, fileName);
+                
+                CurrentlyCreatedFile = FileType.None; //This is set in order not to conflict with cs interface creation or enums 
+                return;
+            }
+            
         }
 
         private static void GenerateCSharpMonobehaviourScript(string metaFilePath, string fileName)
@@ -151,49 +163,52 @@ namespace HappyPixels.EditorAddons
         
         public static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
         {
-            if (!sourcePath.EndsWith(".cs"))
+            if (File.GetAttributes(sourcePath).HasFlag(FileAttributes.Directory))
             {
-                Debug.Log("<color=yellow>Moving none cs file" + sourcePath + "</color>");
-                
-                if (File.GetAttributes(sourcePath).HasFlag(FileAttributes.Directory))
-                {
-                    Directory.Move(sourcePath, destinationPath);
-                    Directory.EnumerateFiles(destinationPath).ToList().ForEach(file =>
-                    {
-                        Debug.Log($"<color=purple>{file}</color>");
-                        if (file.EndsWith(".asmdef"))
-                        {
-                            ChangeOrAddLine(file,
-                                GenerateNamespace(file + ".meta"), "\"name\":");
-                        }
-                        else
-                            ChangeOrAddLine(file,
-                                GenerateNamespace(file + ".meta"), "namespace");
+                MoveFolderWithContent(sourcePath, destinationPath);
+            }
+            else if (sourcePath.EndsWith(".cs"))
+            {
+                ChangeOrAddLine(sourcePath, GenerateNamespace(destinationPath + ".meta"), "namespace");
 
-                    });
-                }
-                else
-                    File.Move(sourcePath, destinationPath);
-                
+                File.Move(sourcePath, destinationPath);
                 File.Move(sourcePath + ".meta", destinationPath + ".meta");
-                return AssetMoveResult.DidMove;
+            }
+            else
+            {
+                File.Move(sourcePath, destinationPath);
+                File.Move(sourcePath + ".meta", destinationPath + ".meta");
             }
             
-            ChangeOrAddLine(sourcePath, GenerateNamespace(destinationPath + ".meta"), "namespace");
-            
-            File.Move(sourcePath, destinationPath);
-            File.Move(sourcePath + ".meta", destinationPath + ".meta");
-                
             return AssetMoveResult.DidMove;
         }
-        
-        //The worst thing about this approach is that we need to read the whole file which is unnecessary...
+
+        private static void MoveFolderWithContent(string sourcePath, string destinationPath)
+        {
+            Debug.Log("<color=yellow> Moving folder with content </color>");
+            Directory.Move(sourcePath, destinationPath);
+            Directory.EnumerateFiles(destinationPath).ToList().ForEach(file =>
+            {
+                Debug.Log($"<color=green>{file}</color>");
+                if (file.EndsWith(".asmdef"))
+                {
+                    //TODO: Change this to ChangeOrAddLine custom method for asemdef files
+                    FileTemplatesGenerators[FileType.Asmdef].Invoke(file + ".meta", Path.GetFileName(file));
+                }
+                else if (file.EndsWith(".cs"))
+                {
+                    ChangeOrAddLine(file, GenerateNamespace(destinationPath + Path.DirectorySeparatorChar), "namespace");
+                }
+            }); 
+            File.Move(sourcePath + ".meta", destinationPath + ".meta");
+        }
+
         private static void ChangeOrAddLine(string filePath, string newLine, string beginningTextLine = "")
         {
             using var fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
             using var sr = new StreamReader(fs);
             using var sw = new StreamWriter(fs);
-            
+
             var lines = sr.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
             fs.Position = 0;
             if (beginningTextLine != "")
